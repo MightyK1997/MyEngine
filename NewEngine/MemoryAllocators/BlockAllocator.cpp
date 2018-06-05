dd86c5022cafe8e868e0586a8c28edd3aecc093e
Sai Upadhyayula <pavankoundinya.upadhyayula@gmail.com>
2018-06-05 17:50:48 -0600

    Adding C++ project

--- NewEngine/MemoryAllocators/BlockAllocator.cpp
+++ NewEngine/MemoryAllocators/BlockAllocator.cpp
@@ -1,1 +1,3 @@
 #include "BlockAllocator.h"
+
+
--- NewEngine/MemoryAllocators/BlockAllocator.h
+++ NewEngine/MemoryAllocators/BlockAllocator.h
@@ -1,11 +1,27 @@
 #pragma once
 
 
-
-
 struct BlockDestriptor
 {
 	void* p_ReturnPointer;
-	size_t m_SizeOfBlock;
-	bool b_IsAllocated;
+	size_t m_SizeOfBlockAndIsBlockFilled;
+	BlockDestriptor* p_NextBlock;
+};
+
+class BlockAllocator
+{
+public:
+	void* _alloc(size_t i_BlockSize);
+	void _free(void* i_pPointer);
+	size_t GetLargestFreeBlock();
+	bool IsBlockFree(void* i_pPointer);
+
+
+
+
+private:
+
+
+	size_t m_TotalMemoryHeapSize;
+	void* p_FirstPointerToHeap;
 };
