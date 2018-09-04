e88e5b5b928a20080e5bb884f0e34f3c1638fbc5
Sai Pavan Koundinya Upadhyayula <u.koundinya@hotmail.com>
2018-09-04 02:14:25 -0600

    Adding all related mesh data to the file and referencing from other files

--- Engine/Graphics/cMesh.h
+++ Engine/Graphics/cMesh.h
@@ -1,6 +1,26 @@
 #pragma once
-
 #include "Graphics.h"
+//#include <Engine/Windows/Includes.h>
+
+#ifdef EAE6320_PLATFORM_D3D 
+#include <d3d11.h>
+#include <dxgi.h>
+#endif
+
+#include "cConstantBuffer.h"
+#include "ConstantBufferFormats.h"
+#include "cRenderState.h"
+#include "cShader.h"
+#include "sContext.h"
+#include "VertexFormats.h"
+
+#include <Engine/Asserts/Asserts.h>
+#include <Engine/Concurrency/cEvent.h>
+#include <Engine/Logging/Logging.h>
+#include <Engine/Platform/Platform.h>
+#include <Engine/Time/Time.h>
+#include <Engine/UserOutput/UserOutput.h>
+#include <utility>
 
 namespace eae6320
 {
@@ -9,10 +29,22 @@ namespace eae6320
 		class cMesh
 		{
 		public:
-			void Initialize();
+			cResult Initialize();
 			void Draw();
 			void Shutdown();
 		private:
+#ifdef EAE6320_PLATFORM_D3D
+			// A vertex buffer holds the data for each vertex
+			ID3D11Buffer* s_vertexBuffer = nullptr;
+			// D3D has an "input layout" object that associates the layout of the vertex format struct
+			// with the input from a vertex shader
+			ID3D11InputLayout* s_vertexInputLayout = nullptr;
+#elif EAE6320_PLATFORM_GL
+			// A vertex buffer holds the data for each vertex
+			GLuint s_vertexBufferId = 0;
+			// A vertex array encapsulates the vertex data as well as the vertex input layout
+			GLuint s_vertexArrayId = 0;
+#endif
 		};
 	}
 }
