155fe3b7ba5a3fc887d33ff0e0c58cd26145cfb1
Sai Pavan Koundinya Upadhyayula <u.koundinya@hotmail.com>
2019-01-22 16:02:59 -0700

    Adding new shaders for assignment 2

--- FinalGame_/Content/AssetsToBuild.lua
+++ FinalGame_/Content/AssetsToBuild.lua
@@ -5,34 +5,12 @@
 return
 {
 	meshes = {
-	"Meshes/Lambo.txt",
-	"Meshes/SmartCar.txt",
-	"Meshes/RaceTrack.txt",
-	"Meshes/TrafficLight.txt",
-	"Meshes/LP_Car1.txt",
-	"Meshes/LP_Car2.txt",
 	"Meshes/Tree.txt",
-	"Meshes/Restart.txt",
-	"Meshes/Numbers/Number1.txt",
-	"Meshes/Numbers/Number2.txt",
-	"Meshes/Numbers/Number3.txt",
-	"Meshes/Numbers/Number4.txt",
-	"Meshes/Numbers/Number5.txt",
-	"Meshes/Numbers/Number6.txt",
-	"Meshes/Numbers/Number7.txt",
-	"Meshes/Numbers/Number8.txt",
-	"Meshes/Numbers/Number9.txt",
-	"Meshes/Numbers/Number0.txt",
 	},
 
 	effects = {
 		"Effects/Effect1.Effect",
 		"Effects/Effect2.Effect",
+		"Effects/Effect3.Effect",
 	},
-
-	sounds = {
-		"Sounds/carstartgarage.wav",
-		"Sounds/bgm.wav",
-		"Sounds/bgm2.wav",
-	}
 }
--- FinalGame_/FinalGame/cFinalGame.cpp
+++ FinalGame_/FinalGame/cFinalGame.cpp
@@ -140,7 +140,7 @@ eae6320::cResult eae6320::cFinalGame::Initialize()
 
 	std::string effectPath = "data/Effects/Effect1.effectbinary";
 	eae6320::Graphics::cEffect::s_Manager.Load(effectPath, effect1Handle);
-	effectPath = "data/Effects/Effect2.effectbinary";
+	effectPath = "data/Effects/Effect3Ass.effectbinary";
 	eae6320::Graphics::cEffect::s_Manager.Load(effectPath, effect2Handle);
 
 	UpdateMeshAndEffect();
