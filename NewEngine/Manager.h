c88179ba8504c909015b0554c5125aeaefd68d68
Sai Upadhyayula <pavankoundinya.upadhyayula@gmail.com>
2019-02-04 20:50:33 -0700

    Adding handles

---
+++ NewEngine/Manager.h
@@ -0,0 +1,35 @@
+#pragma once
+
+
+#include "Handle.h"
+#include <string>
+#include <map>
+#include <Windows.h>
+#include <vector>
+
+
+namespace Handle
+{
+	template<class tAsset, typename tKey = std::string>
+	class Manager
+	{
+	public:
+		HRESULT Load(const tKey& i_Key, Handle<tAsset>& o_Handle);
+		HRESULT Release(Handle<tAsset>*& io_Handle);
+
+
+	private:
+		struct AssetData
+		{
+			tAsset* m_Asset = nullptr;
+			uint64_t id;
+			uint32_t m_ReferenceCount;
+
+			AssetData(tAsset* m_Asset, uint64_t id, uint32_t m_ReferenceCount) :m_Asset(m_Asset), id(id), m_ReferenceCount(m_ReferenceCount) {}
+		};
+
+		std::vector<AssetData> m_AllAssets;
+		std::map<tKey, Handle<tAsset>> m_ListOfAllHandles;
+	};
+}
+
