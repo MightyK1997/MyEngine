dc2a079e28519f29b47f83f243cc3c420dfb4e1f
Sai Upadhyayula <pavankoundinya.upadhyayula@gmail.com>
2019-02-11 15:57:22 -0700

    some fixes

--- NewEngine/Audio/AudioWasapi.cpp
+++ NewEngine/Audio/AudioWasapi.cpp
@@ -4,14 +4,12 @@
 //I've already created a working sound system using XAudio2, which i'll show in the stream later.
 //Let me know in the chat, if the font is too small.
 
+
 AudioWasapi::AudioWasapi()
 {
 
 	HRESULT hr;
 	//First ch;eck for suitable audio endpoint device.
-
-	const CLSID CLSID_mmEnumerator = __uuidof(MMDeviceEnumerator);
-	const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
 	if (FAILED(hr = CoCreateInstance(CLSID_mmEnumerator, NULL, CLSCTX_ALL, IID_IMMDeviceEnumerator
 		, reinterpret_cast<void**>(&pDeviceEnumerator))))
 	{
@@ -49,7 +47,6 @@ HRESULT AudioWasapi::PlayAudioData(const std::string& i_SoundFileName)
 {
 	HRESULT hr = S_OK;
 
-
 	HANDLE soundHandle;
 	Sound::Sound::LoadFileData(i_SoundFileName, soundHandle);
 
@@ -61,9 +58,70 @@ HRESULT AudioWasapi::PlayAudioData(const std::string& i_SoundFileName)
 	//You can either use return hr or EXIT_ON_ERROR(hr). Does the same job.
 	//Get actual buffer size
 	if (FAILED(hr = pAudioClient->GetBufferSize(&mBufferSize))) return hr;
-	if (FAILED(hr = pAudioClient->GetService(IID_IAudioClient, reinterpret_cast<void**>(&pAudioRenderClient)))) return hr;
+	if (FAILED(hr = pAudioClient->GetService(IID_IAudioRenderClient, reinterpret_cast<void**>(&pAudioRenderClient)))) return hr;
+
 
 	//Now we get the buffer in which we store data.
 
+	XAUDIO2_BUFFER tempBuffer;
+
+	Sound::Sound::LoadFileDataIntoBuffer(soundHandle, tempBuffer, *pWaveFormat);
+
+	wavData.pData = tempBuffer.pAudioData;
+	wavData.lengthOfData = tempBuffer.AudioBytes;
+
+	BYTE* pData;
+
+
+	if (FAILED(hr = pAudioRenderClient->GetBuffer(mBufferSize, &pData))) return hr;
+
+		// Load the initial data into the shared buffer.
+	hr = LoadData(mBufferSize, pData);
+
+	hr = pAudioRenderClient->ReleaseBuffer(mBufferSize, flags);
+
+	hnsActualDuration = static_cast<double>(REFTIMES_PER_SEC) *
+		mBufferSize / pWaveFormat->nSamplesPerSec;
+
+	hr = pAudioClient->Start();  // Start playing.
+		// Each loop fills about half of the shared buffer.
+		while (flags != AUDCLNT_BUFFERFLAGS_SILENT)
+		{
+			// Sleep for half the buffer duration.
+			Sleep((DWORD)(hnsActualDuration / REFTIMES_PER_MILLISEC / 2));
+
+			// See how much buffer space is available.
+			hr = pAudioClient->GetCurrentPadding(&mNumberOfPaddingFrames);
+			mNumberOfAvailableFrames = mBufferSize - mNumberOfPaddingFrames;
+
+			// Grab all the available space in the shared buffer.
+			hr = pAudioRenderClient->GetBuffer(mNumberOfAvailableFrames, &pData);
+				// Get next 1/2-second of data from the audio source.
+			hr = LoadData(mNumberOfAvailableFrames, pData);
+
+			hr = pAudioRenderClient->ReleaseBuffer(mNumberOfAvailableFrames, flags);
+		}
+
+	// Wait for last data in buffer to play before stopping.
+	Sleep((DWORD)(hnsActualDuration / REFTIMES_PER_MILLISEC / 2));
+
+	hr = pAudioClient->Stop();  // Stop playing.
+
 	return hr;
+}
+
+HRESULT AudioWasapi::LoadData(uint32_t i_BufferSize, BYTE*& o_Data)
+{
+	HRESULT hr = S_OK;
+	if (m_CurrentPointerLocation + i_BufferSize < wavData.lengthOfData)
+	{
+		memcpy(o_Data, wavData.pData + m_CurrentPointerLocation, i_BufferSize);
+		m_CurrentPointerLocation += i_BufferSize;
+		return hr;
+	}
+	else
+	{
+		flags = AUDCLNT_BUFFERFLAGS_SILENT;
+		return hr;
+	}
 }
--- NewEngine/Audio/AudioWasapi.h
+++ NewEngine/Audio/AudioWasapi.h
@@ -16,11 +16,21 @@
 
 class AudioWasapi
 {
+	struct Data
+	{
+		const BYTE* pData;
+		uint64_t lengthOfData;
+	};
+
 public:
 	AudioWasapi();
 	~AudioWasapi();
 
 	HRESULT PlayAudioData(const std::string& i_SoundFileName);
+
+
+private:
+	HRESULT LoadData(uint32_t i_BufferSize, BYTE*& o_Data);
 
 private:
 	IMMDeviceEnumerator* pDeviceEnumerator;
@@ -33,10 +43,23 @@ private:
 	//using for now.
 	WAVEFORMATEX* pWaveFormat;
 	uint32_t mBufferSize;
+	uint32_t mNumberOfPaddingFrames;
+	uint32_t mNumberOfAvailableFrames;
+	Data wavData;
+	DWORD flags = 0;
+
+	uintptr_t m_CurrentPointerLocation = 0;
 
 	//These are the duration for which the buffer is played. We initialize it
 	//To reference time per second and then update the values.
 	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC;
 	REFERENCE_TIME hnsActualDuration;
+
+
+	//const values
+	const CLSID CLSID_mmEnumerator = __uuidof(MMDeviceEnumerator);
+	const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+	const IID IID_IAudioClient = __uuidof(IAudioClient);
+	const IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
 };
 
