c921f2f7683b74d0cde6581e7e18cb3a83e1cefb
Sai Upadhyayula <pavankoundinya.upadhyayula@gmail.com>
2019-02-11 11:48:05 -0700

    Refactoring

---
+++ Engine/Graphics/RenderCommands.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <cstdint>
+
+
+namespace eae6320
+{
+	namespace Graphics
+	{
+		namespace RenderCommands
+		{
+			enum class RenderCommandTypes : uint8_t
+			{
+				E_DRAWINDEPENDENT = 0x01,
+				E_DRAWDEPENDENT = 0X02,
+			};
+
+			enum class BitMasksForRenderCommands : uint8_t
+			{
+				E_COMMONBITMASK = 7,
+			};
+
+			enum class BitShiftsForRenderCommands :uint8_t
+			{
+				E_EFFECTSHIFT = 57,
+				E_MATERIALSHIFT = 50,
+				E_DEPTHSHIFT = 43,
+				E_TYPESHIFT = 13,
+				E_MESHSHIFT = 7,
+			};
+		}
+	}
+}
